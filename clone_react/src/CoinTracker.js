b772bdf9ca05bb7be74db91d0a806fdf33a56532
Haena <kimhn200605@gmail.com>
2022-07-26 06:57:51 +0900

    feat: CoinTracker (2)

--- clone_react/src/CoinTracker.js
+++ clone_react/src/CoinTracker.js
@@ -11,23 +11,28 @@ function CoinTracker() {
 			.then((response) => response.json())
 			.then((json) => {
 				setCoins(json);
-				console.log(json);
 				setLoading(false);
 			});
 	}, []);
 
 	return (
 		<div>
-			<h1>The Coins!</h1>
-			{loading ? <strong>Loading...</strong> : null}
-			<ul>
-				{coins.map((coin) => (
-					<li>
-						{/* 여기서는 각 coin 마다 고유 id 가 존재하기 때문에 리스트에 굳이 key 지정 X */}
-						{coin.name} {coin.symbol} {coin.quotes.USD.price}
-					</li>
-				))}
-			</ul>
+			{/* 로딩이 끝나면 coin 개수 출력 (템플릿 문자열) */}
+			<h1>The Coins! {loading ? "" : `(${coins.length})`} </h1>
+
+			{/* 로딩중이라면 Loading 문구를 표시하고, 로딩이 끝나면 coins 출력*/}
+			{loading ? (
+				<strong>Loading...</strong>
+			) : (
+				<select>
+					{coins.map((coin) => (
+						<option>
+							{/* 여기서는 각 coin 마다 고유 id 가 존재하기 때문에 리스트에 굳이 key 지정 X */}
+							{coin.name} {coin.symbol} {coin.quotes.USD.price}
+						</option>
+					))}
+				</select>
+			)}
 		</div>
 	);
 }
