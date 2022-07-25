a1390e0991d6433c120d75046890a0e325933ed6
Haena <kimhn200605@gmail.com>
2022-07-26 06:12:42 +0900

    feat: ToDoList (2)

--- clone_react/src/App.js
+++ clone_react/src/App.js
@@ -9,7 +9,7 @@ function App() {
 	};
 
 	const onSubmit = (e) => {
-		e.preventDefault();
+		e.preventDefault(); // submit 기본 동작인 새로고침 방지
 
 		if (toDo === "") {
 			// 아무것도 입력하지 않았다면 바로 리턴
@@ -21,7 +21,6 @@ function App() {
 		setToDo(""); // 입력란 초기화
 	};
 
-	toDos.map((item, index) => <li key={index}>{item}</li>);
 	return (
 		<div>
 			<h1>My To Dos ({toDos.length})</h1>
@@ -36,6 +35,12 @@ function App() {
 				{/* form 내부의 버튼 하나는 자동으로 submit 기능 수행 */}
 				<button>Add To Do</button>
 			</form>
+			<hr />
+			<ul>
+				{toDos.map((item, index) => (
+					<li key={index}>{item}</li>
+				))}
+			</ul>
 		</div>
 	);
 }
